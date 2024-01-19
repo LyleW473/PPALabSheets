43f1113d1038faa85e68ca144366cf3cd31b2942
Lyle Wong <wlyle473@gmail.com>
2024-01-19 18:03:05 +0000

    Separating attributes and methods into sections

--- Labsheet8/LGTExercises/Ex3/Automobile.java
+++ Labsheet8/LGTExercises/Ex3/Automobile.java
@@ -4,35 +4,52 @@ public interface Automobile {
 
 	public enum WheelPosition {LR,LF,RR,RF};
 	public enum SeatPosition {Forward, Back};
-	
+
+    // Main vehicle functionality
 	public void drive();
 	public void start();
 	public void stop();
-	public void changeTyre(Tyre newTyre, WheelPosition pos);
-	public Tyre getTyre(WheelPosition pos);
-	public void wash();
-	public boolean checkOil();
-	public int getOil();
-	public void changeGear(int gear);
-	public void accelerate();
-	public void addOil(int oil);
-	public void changeWasherLiquid();
-	public void changeBrakeFluid();
 	public void brake();
-	public void addPetrol(int petrol);
-	public int getPetrol();
+    public void accelerate();
+    public void changeGear(int gear);
+
+    // Additional car-related functionality
 	public void setSeatPosition(int seat, SeatPosition pos);
 	public SeatPosition getSeatPosition(int seat);
+
 	public void openBoot();
 	public void closeBoot();
-	public void openBonnet();
-	public void closeBonnet();
+
 	public void setRadioStation(int station);
 	public void powerRadio(boolean power);
+
+    // Maintenance-related
+	public int getOil();
+    public boolean checkOil();
+	public void addOil(int oil);
+
+	public void addPetrol(int petrol);
+	public int getPetrol();
+
+	public void changeTyre(Tyre newTyre, WheelPosition pos);
+	public Tyre getTyre(WheelPosition pos);
+
+	public void wash();
+	public void changeWasherLiquid();
+
+	public void changeBrakeFluid();
+
 	public void changeAirFilter();
-	public float getEmissionsData();
+
+	public void openBonnet();
+	public void closeBonnet();
+
+    // External to automobile
 	public void setTaxi(boolean isTaxi);
 	public boolean isTaxi();
+
 	public void setMOTCertificate(boolean mot);
 	public boolean hasMOTCertificate();
+
+    public float getEmissionsData();
 }
