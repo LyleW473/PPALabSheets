acf7e04e171e2ccd9547d9d013cd6fb7515ddb1e
Lyle Wong <wlyle473@gmail.com>
2024-01-19 17:53:19 +0000

    Completed Ex 2

--- Labsheet8/FoodPack.java
+++ Labsheet8/FoodPack.java
@@ -1,28 +1,56 @@
 public class FoodPack {
-    private static final int MAX_EFFECT;
+    private static final int MAX_EFFECT = 100;
     private Location location;
 
     private int effect;
 
-    public void FoodItem(){
-        MAX_EFFECT = 100;
+    public static void main(String[] args) {
+
+        float x = 9;
+        float y = 5;
+        float z = 2;
+        Location location = new Location(x, y, z);
+        FoodPack myFoodPack = new FoodPack(location, 50);
+
+        System.out.println(myFoodPack.getEffect());
+        
+        myFoodPack.setEffect(20023020);
+        System.out.println(myFoodPack.getEffect());
+
+        myFoodPack.setEffect(2);
+        System.out.println(myFoodPack.getEffect());
+        
+        myFoodPack.setLegendary();
+        System.out.println(myFoodPack.getEffect());
+
+        System.out.println(myFoodPack.getLocation().toString());
     }
 
-    private FoodItem(Location loc, int eff) {
-        MAX_EFFECT = 100;
+    public FoodPack(Location loc, int eff) {
         this.location = loc;
         this.effect = eff;
     }
 
-    public int getEffect(int effect) {
+    public Location getLocation() {
+        return location;
+    }
+
+    public int getEffect() {
         return effect;
     }
 
     public void setEffect(int effect) {
-        this.effect = effect;
+        if (effect < MAX_EFFECT)
+        {
+            this.effect = effect;
+        }
+        else
+        {
+            this.effect = MAX_EFFECT;
+        }
     }
 
     public void setLegendary() {
-        MAX_EFFECT = 200;
+        effect = MAX_EFFECT;
     }
 }
