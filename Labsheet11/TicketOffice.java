a51dec8619e962b25855554eb6297fdf8d56fd0c
Lyle Wong <wlyle473@gmail.com>
2024-01-21 01:53:29 +0000

    Added "TicketOffice" class: - Added empty methods (that need to be implemented)

---
+++ Labsheet11/TicketOffice.java
@@ -0,0 +1,30 @@
+public class TicketOffice 
+{
+    private int numScreens;
+
+    public TicketOffice(int numScreens)
+    {
+        this.numScreens = numScreens;
+    }
+
+    public void addScreen(int screenID, String numRows, String numCols)
+    {
+    }
+
+    public void findScreen(int screenID)
+    {
+    }
+
+    public void showNewMovie(int screenID, String movieTitle, double movieCost)
+    {
+    }
+    
+    public void showMovies()
+    {
+    }
+
+    public Screen bookRandomTicket(String movieTitle)
+    {
+        return null;
+    }
+}
