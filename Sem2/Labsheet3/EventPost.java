ca54edda06973cdf26c731b4ab3e4265aad8cd64
Lyle Wong <wlyle473@gmail.com>
2024-02-04 02:35:49 +0000

    Changes made: - Added comments to "EventPost" class. - Removed unused imports in classes.

--- Sem2/Labsheet3/EventPost.java
+++ Sem2/Labsheet3/EventPost.java
@@ -1,17 +1,32 @@
 public class EventPost extends Post
 {
-    private String description;
+    private String description; // A description of the event that has occured, mentioned in the event post.
 
+    /**
+     * Constructor for objects of class PhotoPost.
+     * 
+     * @param author    The username of the author of this post.
+     */
     public EventPost(String author)
     {
         super(author);
     }
 
+    /**
+     * Add a description to this event post.
+     * 
+     * @param description The description to be added.
+     */
     public void addDescription(String description)
     {
         this.description = description;
     }
 
+    /**
+     * Return the description of the event post.
+     * 
+     * @return The description of the event post.
+     */
     public String getDescription()
     {
         return description;
